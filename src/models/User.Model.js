caa62ed8a6ee53632aa8551f8ca1e9e87d7bd38e
Arman <arman.gupta254@gmail.com>
2026-03-19 15:56:03 +0530

    write utilities templates

---
+++ src/models/User.Model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import {Schema} from mongoose;
+
+const userSchema= new Schema({
+    username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // will store in cloudnary and store its url
+            required: true,
+        },
+        coverImage: {
+            type: String, // only url of uploaded image in cloudnary
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+},{
+    timestamps:true
+})
+
+
+export const User = mongoose.model("User", userSchema)
